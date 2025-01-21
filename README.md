b9a153edef730c4119a72e8458fe75ee9b4783ac
Sirikan-P <sirikan.interior@gmail.com>
2025-01-21 15:43:03 +0700

    router

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 npm create vite .
 npm install
 npm run dev
+
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
 ```
 
 ## Step 2 git 
@@ -16,7 +19,12 @@ git push
 
 ### when update git
 ```bash
-git add.
+git add .
 git commit -m "init"
 git push
+```
+
+## Step 3 install React Router 
+```bash
+npm i react-router
 ```
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,46 @@
 //rfce
 import React from 'react'
+import { Link, Route,Routes } from 'react-router'
 
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+
+      <nav className="flex 
+                    justify-between 
+                    h-8
+                    bg-green-300
+                    px-4
+                    items-center *:
+                    ">
+        <div className="flex gap-4"> 
+          <Link to='/'> Home </Link> 
+          <Link to='about'> About </Link> 
+          <Link to='contact'> Contact </Link> 
+        </div>
+        <div className="flex gap-4"> 
+          <Link to='login'> Login </Link> 
+          <Link to='register'> Register </Link> 
+        </div>
+      </nav>
+
+      <Routes>
+      {/* public */}
+            <Route path="/" element= { <h1>Home</h1>}  />
+            <Route path="about" element={ <h1>About</h1>} />
+            <Route path="contact" element={ <h1>Contact</h1>} />
+            <Route path="login" element={ <h1>Login Page</h1>} />
+            <Route path="register" element={ <h1>Register</h1>} />
+
+      {/* private */}
+            {/* parent/child  */}
+          <Route path="admin"> 
+            <Route path="dashboard" element={ <h1>Dashboard Page</h1> }/> 
+            <Route path="manage" element={ <h1>Manage Page</h1> } />
+            <Route path="setting" element={ <h1>Setting Page</h1>}/>
+          </Route>
+      </Routes>
+    </div>
   )
 }
 
